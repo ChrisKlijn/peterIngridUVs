84ab8f38d7f0abfe23bdd7a07c2231ad114c9091
Christiaan Klijn <c.klijn@gmail.com>
2011-10-13 14:56:38 +0000

    Work on pairwise CGH and dose-response curves
    git-svn-id: http://bioinformatics.nki.nl/svn/IHD-R-BioC-packages/trunk/codeChris/smallProjects/peterIngridUV@724 e9328c4f-6670-0410-aa1d-bbebe5fdda6e

--- UV_sandbox.R
+++ UV_sandbox.R
@@ -5,22 +5,27 @@
 # Description: Sandbox code
 # -------------------------------------------------------------------
 
-# WD - medoid
-
-setwd("/home/klijn/data/smallproj/UVs")
+# Medoid
 
 # Libraries and functions
 library(reshape)
 library(drc)
 source('~/codeChris/smallProjects/peterIngridUV/UV_functions.R')
 
-# Load files
+# Load raw files
 
 setwd("/home/klijn/data/smallproj/UVs/rawData")
 filesIC50 <- dir(pattern='.txt')
-
 IC50List <- loadIC50files(filevect=filesIC50)
+
+# Background subtraction and data shaping
 IC50List.corr <- lapply(IC50List, correctIC50)
+
+# Load sampleinfo
+
+setwd("/home/klijn/data/smallproj/UVs")
+sampleInfo <- read.delim('sampleinfo.csv', stringsAsFactors=F, sep=',')
+
 
 fitList <- fitDualResponseCurve(targetFrame=IC50List.corr[[1]], controlFrame=IC50List.corr[[2]])
 
@@ -31,8 +36,25 @@ plotDualResponseCurve(fitList)
 
 # test control hybs
 
-a <- IC50List.corr[grep('RMCE', names(IC50List.corr))]
-b <- lapply(a, function (x) { drm(response ~ conc, data=x, fct=LL.2())})
+RMCE <- IC50List.corr[grep('RMCE', names(IC50List.corr))]
+RMCE.fit <- lapply(RMCE, function (x) { drm(response ~ conc, data=x, fct=LL.2())})
 x11()
-plot(b[[1]], type='n')
-lapply(b, plotSingleResponseCurve)
+plot(RMCE.fit[[1]], type='n', main='RMCE dose response curves')
+lapply(RMCE.fit, plotSingleResponseCurve)
+
+# test control hybs
+
+Brca1 <- IC50List.corr[grep('Brca1', names(IC50List.corr))]
+Brca1.fit <- lapply(Brca1, function (x) { drm(response ~ conc, data=x, fct=LL.2())})
+x11()
+plot(Brca1.fit[[1]], type='n', main='Brca1 dose response curves')
+lapply(Brca1.fit, plotSingleResponseCurve)
+
+RMCE.IC50 <- lapply(RMCE.fit, function(x) {
+  return(coef(x)['e:(Intercept)'])})
+Brca1.IC50 <- lapply(Brca1.fit, function(x) {
+  return(coef(x)['e:(Intercept)'])})
+plot(RMCE.IC50, Brca1.IC50, pch=19, col=colors()[108], 
+  main='IC50s of Brca1 and RMCE experiments', xlim=c(0,.3), ylim=c(0,.3))
+
+  
